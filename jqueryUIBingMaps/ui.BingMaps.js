062eac9a59b595a3800a2b651634955d436c6170
Alan Lai <anlai@ucdavis.edu>
2010-12-06 11:39:13 -0800

    added auto show info box when adding pin option

--- jqueryUIBingMaps/ui.BingMaps.js
+++ jqueryUIBingMaps/ui.BingMaps.js
@@ -17,10 +17,13 @@
             crosshairLocation: "crosshair.gif", /* location of the crosshair image file */
             displayZoom: false,                 /* displays the current zoom level */
             displayZoomControl: undefined,      /* control to set zoom if you have a specific control to put the zoom level in */
-            displaySearch: false, 			/* displays search controls */
+            displaySearch: false, 				/* displays search controls */
             locationTitle: "Current Location",
             searchTitle: "Search",
-            coordinateTitle: "Locations"
+            coordinateTitle: "Locations",
+			loadAllPins: false,					/* initially load all pins */
+			hideCoordinates: false,				/* hides all the coordinates */
+			autoShowInfoBox: false				/* automatically show info boxes when pins are added */			
         },
         _create: function() {
             this.id = this.element.attr("id");
@@ -55,6 +58,11 @@
 
                 this._registerSearch();
             }
+			
+			if (this.options.hideCoordinates)
+			{
+				this.coordinates.hide();
+			}
         },
         _formatCoordinates: function($coordinateContainer /* div holding dl */) {
             $coordinateContainer.addClass("coordinate-container");
@@ -71,12 +79,21 @@
 
             this.veMap = veMap;
 
-            // set the default pin if there is one			
-            var defaultCoordinate = this.coordinates.find("div.default-location");
-            if (defaultCoordinate.length > 0) {
-                //AddPushPin(map, $coordinates, defaultCoordinate);
-                this._addPushPin(defaultCoordinate);
-            }
+			
+            // set the default load pins
+			if (this.options.loadAllPins)
+			{
+				var locations = this.coordinates.find("div.map-button");
+				var that = this;
+				$.each(locations, function(index, item){ that._addPushPin($(item)); });
+			}
+			else // load single default location if there is one
+			{
+				var defaultCoordinate = this.coordinates.find("div.default-location");
+				if (defaultCoordinate.length > 0) {
+					this._addPushPin(defaultCoordinate);
+				}
+			}
         },
         _addPushPin: function($button) {
             // prepare a pin to be added to the map
@@ -106,6 +123,11 @@
 				this._handleShowBestFit();
 			}
 
+			if (this.options.autoShowInfoBox)
+			{
+				this.veMap.ShowInfoBox(vePin);
+			}
+			
         },
         _handleRouting: function($button, veLocation, vePin) {
             var src = this.coordinates.find("div.src");
@@ -210,10 +232,10 @@
 			var that = this;
 			$.each(selected, function(index,item){ locations.push(that._readLatLng($(item))); });
 			
-			if (locations.length != 0)
-			{
-				this.veMap.SetMapView(locations);
-			}
+			// show best fit for multiple pins
+			if (locations.length > 1) this.veMap.SetMapView(locations);
+			// show standard zoom for 1 pin
+			if (locations.length == 1) this.veMap.SetCenterAndZoom(locations[0], 16);
 		},
 		_readLatLng: function($button) {
 			return new VELatLong($button.attr("lat"), $button.attr("lng"));		
