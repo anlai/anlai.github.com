64cf10310c992db77c91b95f74748c6f1f89b71e
Alan Lai <anlai@ucdavis.edu>
2011-06-28 15:38:21 -0700

    cleaned up the plugin

--- jqueryGPositions/jquery.gPositions.js
+++ jqueryGPositions/jquery.gPositions.js
@@ -31,6 +31,7 @@ var MapMode = { STANDARD: 0, ROUTING: 1, SELECTINGPOINT: 2 };
         gPositions : function(options) {
 		
 			var markers = [];
+			var alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 			// required to use the direction/routing services
 			var directionsService = new google.maps.DirectionsService();
 			var directionDisplay;
@@ -43,7 +44,9 @@ var MapMode = { STANDARD: 0, ROUTING: 1, SELECTINGPOINT: 2 };
 				longitude:	-121.749174,
 				zoom:		16,
 				mode:		MapMode.STANDARD,
-				loadAll:	false,	// default to load all pins
+				loadAll:	false,	// default to not load all pins,
+				displayDirections:	false,
+				routeType:	google.maps.TravelMode.WALKING,
 				mapType:	google.maps.MapTypeId.ROADMAP,
 				debug: 		false
 			}, options);
@@ -52,58 +55,262 @@ var MapMode = { STANDARD: 0, ROUTING: 1, SELECTINGPOINT: 2 };
 			
 				var $gpContainer = $(this);	// the map container
 				// create the map object
-				var gmap = CreateMap($gpContainer);
+				var gmap = initializeMap($gpContainer);
 			
-				if (settings.mode == MapMode.STANDARD || settings.mode == MapMode.ROUTING)
+				// initialize the side information container
+				initializeSideContainer($gpContainer, gmap);
+			
+				// attach events to the marker selctors
+				initializeEvents($gpContainer, gmap);
+			
+				// set default coordinates
+				defaultCoordinates($gpContainer, gmap);
+			});
+		
+		
+			/*****************************************************************
+			Initialization functions
+			*****************************************************************/
+			// initializes the google maps object
+			function initializeMap($gpContainer)
+			{
+				var latlng = new google.maps.LatLng(settings.latitude, settings.longitude);
+				var options = { zoom: settings.zoom, center: latlng, mapTypeId: settings.mapType }
+			
+				var map = new google.maps.Map($gpContainer.find(".gp-map")[0], options);
+			
+				// insert the cross hair image
+				if (settings.mode == MapMode.SELECTINGPOINT)
 				{
-					var $coordinates = $gpContainer.find(".gp-coordinate");
+					var crosshair = $("<div>").addClass("gp-crosshair");
+					$gpContainer.find(".gp-map").append(crosshair);
+				}
+			
+				return map;
+			}
+		
+			/*
+				Initialzes the side information container
+			*/
+			function initializeSideContainer($gpContainer, gmap)
+			{
+				var $sideContainer = $("<div>").addClass("gp-sidecontainer");
+				$gpContainer.append($sideContainer);
+			
+				switch(settings.mode)
+				{
+					case MapMode.STANDARD:
 					
-					// create the coordinate list for the user to select from
-					CreateCoordinateList($gpContainer, $coordinates, gmap);
-					BindCoordinateActions($gpContainer, $coordinates, gmap);	
+						// put all the coordinates into the container
+						$sideContainer.append($gpContainer.find(".gp-coordinate"));
+					
+						break;
+					case MapMode.ROUTING:
+					
+						// put all the coordinates into the container
+						$sideContainer.append($gpContainer.find(".gp-coordinate"));
+					
+						if(settings.displayDirections)
+						{
+							$gpContainer.append($("<div>").addClass("gp-directions"));
+						}
+					
+						break;
+					case MapMode.SELECTINGPOINT:
+					
+						// add the standard controls
+						var $ctlList = $("<ul>");
+						$sideContainer.append($ctlList);
+											
+						// add the debug controls
+						if (settings.debug)
+						{
+							var $debug = $("<fieldset>").addClass("gp-debug").append($("<legend>").html("Debug Coordinates"));
+							$sideContainer.append($debug);
+							
+							// add in the controls
+							var $list = $("<ul>");
+							$list.append($("<li>").html("Latitude:&nbsp;").append($("<span>").addClass("gp-lat-debug")));
+							$list.append($("<li>").html("Longitude:&nbsp;").append($("<span>").addClass("gp-lng-debug")));
+							
+							$debug.append($list);
+						}
+
+						// create the search boxes
+						var $search = $("<li>").html("Search Address: &nbsp");
+						// create search box
+						var $searchBox = $("<input>").addClass("gp-search-box");
+						$searchBox.keyup(function(event){if(event.keyCode == 13){ $(this).siblings('input[type="button"]').click(); }});
+						// create search button
+						var $searchBtn = $("<input>").attr("type", "button").val("Search");
+						$searchBtn.click(function(){ 
+							var address = $(this).siblings(".gp-search-box").val(); 
+							Search(gmap, address, $sideContainer);
+						});
+						$search.append($searchBox).append($searchBtn);
+						
+						$ctlList.append($search);
+						$ctlList.append($("<input>").attr("type", "hidden").attr("name", "gp-lat").addClass("gp-lat"));
+						$ctlList.append($("<input>").attr("type", "hidden").attr("name", "gp-lng").addClass("gp-lng"));
+						
+						break;
+					default:
+						alert("invalid mode has been selected");
+						break;
+				}
+			
+			}
+		
+			// attaches event handlers where needed
+			function initializeEvents($gpContainer, gmap)
+			{
+			
+				switch(settings.mode)
+				{
+					case MapMode.STANDARD:
+					
+						// event to add marker to map
+						$gpContainer.find(".gp-coordinate").click(function(){
+							MarkerHandler(gmap, $(this));
+							AdjustMapView($gpContainer, gmap);
+						});
+					
+						break;
+						
+					case MapMode.ROUTING:
+						
+						// event to add marker to map
+						$gpContainer.find(".gp-coordinate").click(function(){
+							MarkerHandler(gmap, $(this));
+							AdjustMapViewRouting($gpContainer, gmap);
+						});
+						
+						break;
+					case MapMode.SELECTINGPOINT:
+					
+						var $side = $gpContainer.find(".gp-sidecontainer");
+					
+						var $lat = $side.find(".gp-lat");
+						var $lng = $side.find(".gp-lng");
+
+						var $latdebug = $side.find(".gp-lat-debug");
+						var $lngdebug = $side.find(".gp-lng-debug");
+					
+						// add the listener
+						google.maps.event.addListener(gmap, 'mouseup', function(event) {
+						
+							$lat.val(event.latLng.lat());
+							$lng.val(event.latLng.lng());
+					
+							// display the information for debug
+							if (settings.debug)
+							{
+								$latdebug.html(event.latLng.lat());
+								$lngdebug.html(event.latLng.lng());
+							}
+				
+						});
+					
+						break;
+					default:
+						alert("invalid mode has been selected");
+						break;
+				}
+			
+			}
+		
+			/*****************************************************************
+			Modification Functions
+			*****************************************************************/
+			// determines whether to add a marker or remove
+			function MarkerHandler(gmap, $selector)
+			{
+				if ($selector.hasClass("gp-selected"))
+				{
+					RemoveMarker($selector);
 				}
 				else
 				{
-					CreatePositionSelectingControls(gmap, $gpContainer);
-					CreatePositionSelecting(gmap, $gpContainer);
+					AddMarker(gmap, $selector);
 				}
-			});
-		
-			/*
-				Adjusts the map to show routing information
-			*/
-			function AdjustMapViewRouting($gpContainer, gmap)
-			{
-				if (directionDisplay != null) directionDisplay.setMap(null);
-			
-				// add the directions overlay to the map
-				directionDisplay = new google.maps.DirectionsRenderer();
-				directionDisplay.setMap(gmap);
-				
-				// figure out the directions
-				var src = markers[0];				// first marker
-				var dest = markers[markers.length-1];	// last marker
-				
-				var wayPts = [];
-				if (markers.length > 2)
-				{		
-					for(i = 1; i < markers.length-1; i++)
-					{
-						wayPts.push({location: markers[i].position, stopover: true});
-					}
-				}
-				
-				var request = {origin: src.position, destination: dest.position, waypoints: wayPts, travelMode: google.maps.TravelMode.WALKING};
-				
-				directionsService.route(request, function(result, status)
-					{
-						if (status == google.maps.DirectionsStatus.OK)
-						{
-							directionDisplay.setDirections(result);
-						}
-					});
 			}
 		
+			/*
+				Creates the marker
+				
+				$selector: the div that holds the mapping information
+				return: returns the gmid number
+			*/
+			function AddMarker(gmap, $selector)
+			{
+				$selector.addClass("gp-selected");
+			
+				var lat = $selector.data("lat");
+				var lng = $selector.data("lng");
+				var latlng = new google.maps.LatLng(lat, lng);
+				
+				var title = $selector.find(".gp-name").html();
+				var description = $selector.find(".gp-description").html();
+			
+				var marker = new google.maps.Marker({
+					position: latlng, 
+					map: gmap, 
+					animation: google.maps.Animation.DROP,
+					title: title != undefined && title != "" ? title : "no title"
+				});  
+				
+				// add the info window when the marker is clicked
+				var infowindow = new google.maps.InfoWindow({
+					content: description
+				});
+
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.open(gmap,marker);
+				});
+			
+				// for routing we don't want to see this pin
+				if (settings.mode == MapMode.ROUTING) 
+				{
+					marker.setMap(null);
+				}
+			
+				markers.push(marker);
+					
+				$selector.data("gmid", marker.__gm_id);
+			}
+			
+			function RemoveMarker($selector)
+			{								
+				$selector.removeClass("gp-selected");
+				var gmid = $selector.data("gmid");
+			
+				// remove the marker from the map				
+				var marker, index;
+				
+				for(i = 0; i < markers.length; i++)
+				{
+					if (markers[i].__gm_id == gmid)
+					{
+						index = i;
+						//marker = markers[i];
+						break;
+					}
+				}
+			
+				if (index != undefined)
+				{
+					//markers.remove(index);
+					marker = markers.splice(index,1);
+					marker[0].setMap(null);
+				}
+			}
+			
+			/*****************************************************************
+			View Functions
+			*****************************************************************/
+			/*
+				Adjusts view of map to deal with all pins that are on map
+			*/
 			function AdjustMapView($gpContainer, gmap)
 			{			
 				var bounds = new google.maps.LatLngBounds();
@@ -150,167 +357,107 @@ var MapMode = { STANDARD: 0, ROUTING: 1, SELECTINGPOINT: 2 };
 				}
 			}
 		
-			// Creates the standard map with selecting points / routing
-			function CreateMap($gpContainer)
-			{			
-				var latlng = new google.maps.LatLng(settings.latitude, settings.longitude);
-				var options = { zoom: settings.zoom, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP }
-			
-				var map = new google.maps.Map($gpContainer.find(".gp-map")[0], options);
-			
-				return map;
-			}
-		
-			function CreateCoordinateList($gpContainer, $coordinates, gmap)
+			/*
+				Adjusts the map to show routing information
+			*/
+			function AdjustMapViewRouting($gpContainer, gmap)
 			{
-				var hasDefault = false;
+				if (directionDisplay != null) directionDisplay.setMap(null);
 			
-				var $list = $("<ul>").addClass("gp-coordinate-container");
-				//var $container = $("<div>").addClass("gp-coordinate-container").append($list);
+				// add the directions overlay to the map
+				directionDisplay = new google.maps.DirectionsRenderer();
+				directionDisplay.setMap(gmap);
 				
-				$.each($coordinates, function(index,item){
+				// figure out the directions
+				var src = markers[0];				// first marker
+				var dest = markers[markers.length-1];	// last marker
 				
-					$list.append($("<li>").append(item));
-				
-					if (settings.loadAll)
-					{					
-						AddMarker(gmap, $(item));
-					}
-					// load in the default pin if specified
-					else
+				var wayPts = [];
+				if (markers.length > 2)
+				{		
+					for(i = 1; i < markers.length-1; i++)
 					{
-						if ($(item).hasClass("gp-default"))
-						{
-							AddMarker(gmap, $(item));							
-							hasDefault = true;
-						}
+						wayPts.push({location: markers[i].position, stopover: true});
 					}
-					
-				});
-				
-				$gpContainer.append($list);
-				
-				if (settings.loadAll || hasDefault)
-				{
-					AdjustMapView($gpContainer, gmap);
 				}
+				
+				var request = {origin: src.position, destination: dest.position, waypoints: wayPts, travelMode: settings.routeType};
+				
+				directionsService.route(request, function(result, status)
+					{				
+						if (status == google.maps.DirectionsStatus.OK)
+						{
+							if (settings.displayDirections) {DisplayDirections($gpContainer, result)};
+							directionDisplay.setDirections(result);
+						}
+					});
 			}
 		
 			/*
-				Creates the marker
-				
-				$selector: the div that holds the mapping information
-				return: returns the gmid number
+				Outputs the directions list
 			*/
-			function AddMarker(gmap, $selector)
-			{
-				$selector.addClass("gp-selected");
-			
-				var lat = $selector.data("lat");
-				var lng = $selector.data("lng");
-				var latlng = new google.maps.LatLng(lat, lng);
-				
-				var title = $selector.find(".gp-name").html();
-				var description = $selector.find(".gp-description").html();
-			
-				var marker = new google.maps.Marker({
-					position: latlng, 
-					map: gmap, 
-					animation: google.maps.Animation.DROP,
-					title: title != undefined && title != "" ? title : "no title"
-				});  
-				
-				// add the info window when the marker is clicked
-				var infowindow = new google.maps.InfoWindow({
-					content: description
-				});
-
-				google.maps.event.addListener(marker, 'click', function() {
-					infowindow.open(gmap,marker);
-				});
-			
-				if (settings.mode == MapMode.ROUTING) 
-				{
-					marker.setMap(null);
-				}
-			
-				markers.push(marker);
-					
-				$selector.data("gmid", marker.__gm_id);
-			}
-			
-			function RemoveMarker($selector)
-			{								
-				$selector.removeClass("gp-selected");
-				var gmid = $selector.data("gmid");
-			
-				// remove the marker from the map				
-				var marker, index;
+			function DisplayDirections($gpContainer, results)
+			{			
+				var $directions = $gpContainer.find(".gp-directions");
+				$directions.empty();
+				var $list = $("<ul>");
 				
 				for(i = 0; i < markers.length; i++)
 				{
-					if (markers[i].__gm_id == gmid)
-					{
-						index = i;
-						//marker = markers[i];
-						break;
-					}
+					// split in the mark icon here too
+					$list.append($("<li>").html(markers[i].title));
 				}
-			
-				if (index != undefined)
+				
+				$directions.append($list);
+			}
+		
+			/*****************************************************************
+			Load Functions
+			*****************************************************************/
+			// load the default coordinates
+			function defaultCoordinates($gpContainer, gmap)
+			{
+				var defaultcoord = $gpContainer.find(".gp-sidecontainer .gp-default");
+				
+				if (defaultcoord[0] != undefined)
 				{
-					//markers.remove(index);
-					marker = markers.splice(index,1);
-					marker[0].setMap(null);
+					AddMarker(gmap, $(defaultcoord[0]));
+					AdjustMapView($gpContainer, gmap);
 				}
 			}
 		
-			function BindCoordinateActions($gpContainer, $coordinates, gmap)
-			{			
-				$coordinates.click(function(){
-					
-					if ($(this).hasClass("gp-selected"))
+			function Search(gmap, address, $resultContainer)
+			{		
+				var geocoder = new google.maps.Geocoder();
+				geocoder.geocode({'address': address}, function(results, status){
+					if (status == google.maps.GeocoderStatus.OK)
 					{
-						RemoveMarker($(this));
-					}
-					else
-					{
-						AddMarker(gmap, $(this));
-					}
-					
-					AdjustMapView($gpContainer, gmap);
-				});
-			}
-			
-			// Creates the map with listener for selecting position
-			function CreatePositionSelecting(gmap, $gpContainer)
-			{			
-				// load in the crosshairs			
-				var $crosshairs = $("<div>").addClass("gp-crosshair");
-				$gpContainer.find(".gp-map").append($crosshairs);		
-							
-				// find the display controls
-				var $list = $gpContainer.find(".gp-selecting-controls");
-				var $lat = $list.find(".gp-lat");
-				var $lng = $list.find(".gp-lng");
-
-				var $latdebug = $list.find(".gp-lat-debug");
-				var $lngdebug = $list.find(".gp-lng-debug");
+						gmap.setCenter(results[0].geometry.location);
 						
-				google.maps.event.addListener(gmap, 'mouseup', function(event) {
-				
-					$lat.val(event.latLng.lat());
-					$lng.val(event.latLng.lng());
-			
-					// display the information for debug
-					if (settings.debug)
-					{
-						$latdebug.html(event.latLng.lat());
-						$lngdebug.html(event.latLng.lng());
+						// set the location
+						var $latInput = $resultContainer.find(".gp-lat");
+						var $lngInput = $resultContainer.find(".gp-lng");
+						
+						$latInput.val(results[0].geometry.location.lat());
+						$lngInput.val(results[0].geometry.location.lng());
+						
+						if (settings.debug)
+						{
+							var $latdebug = $resultContainer.find(".gp-lat-debug");
+							var $lngdebug = $resultContainer.find(".gp-lng-debug");
+							
+							$latdebug.html(results[0].geometry.location.lat());
+							$lngdebug.html(results[0].geometry.location.lng());
+						}
+						
 					}
-			
 				});
 			}
+		
+		
+		
+
+			
 			
 			function CreatePositionSelectingControls(gmap, $gpContainer)
 			{
@@ -327,18 +474,7 @@ var MapMode = { STANDARD: 0, ROUTING: 1, SELECTINGPOINT: 2 };
 				}
 				
 								
-				// create the search boxes
-				var $search = $("<li>").html("Search Address: &nbsp");
-				// create search box
-				var $searchBox = $("<input>").addClass("gp-search-box");
-				$searchBox.keyup(function(event){if(event.keyCode == 13){ $(this).siblings('input[type="button"]').click(); }});
-				// create search button
-				var $searchBtn = $("<input>").attr("type", "button").val("Search");
-				$searchBtn.click(function(){ 
-					var address = $(this).siblings(".gp-search-box").val(); 
-					Search(gmap, address, $lat, $lng);
-				});
-				$search.append($searchBox).append($searchBtn);
+				
 				
 				$list.append($search);
 				$list.append($lat);
@@ -347,33 +483,7 @@ var MapMode = { STANDARD: 0, ROUTING: 1, SELECTINGPOINT: 2 };
 				$gpContainer.append($list);
 			}
 			
-			function Search(gmap, address, $lat, $lng)
-			{
-				var geocoder = new google.maps.Geocoder();
-				geocoder.geocode({'address': address}, function(results, status){
-					if (status == google.maps.GeocoderStatus.OK)
-					{
-						gmap.setCenter(results[0].geometry.location);
-						
-						// set the location
-						var $latInput = $lat.find(".gp-lat");
-						var $lngInput = $lat.find(".gp-lng");
-						
-						$latInput.val(results[0].geometry.location.lat());
-						$lngInput.val(results[0].geometry.location.lng());
-						
-						if (settings.debug)
-						{
-							var $latdebug = $lat.find(".gp-lat-debug");
-							var $lngdebug = $lng.find(".gp-lng-debug");
-							
-							$latdebug.html(results[0].geometry.location.lat());
-							$lngdebug.html(results[0].geometry.location.lng());
-						}
-						
-					}
-				});
-			}
+
 		}
 		
     }); // end of $.fn.extend
